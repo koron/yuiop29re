05706ecb3143692a6c58fff061d4498eb72d1b97
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-09-27 21:33:27 +0900

    documentation about rotary encoder driver

--- re_mon/main.c
+++ re_mon/main.c
@@ -3,6 +3,43 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+// This driver encodes the states of the A and B pins of the rotary encoder
+// into a 2-bit word and checks the past 4 words of history and the current 1
+// word to determine if the rotation was successful and the direction of the
+// rotation.
+//
+// The EC12 compatible rotary encoder operates in the sequence of A connection,
+// B connection, A disconnection, B disconnection when rotating clockwise. In
+// the counterclockwise direction, the sequence is reversed, becoming B
+// connection, A connection, B disconnection, A disconnection. This driver
+// assumes that C is connected to GND and A and B are in a pulled-up state.
+// Therefore, both A and B are in a Hi state initially, and it is expected that
+// they will become Lo states upon connection and Hi states upon disconnection.
+//
+// This driver encodes the Hi state as 0 and the Lo state as 1 when encoding
+// the A and B quantity pins to a word. It also assigns A to the 0th bit and B
+// to the 1st bit. The resulting 2-bit word and the corresponding relationship
+// with the A and B pins are as shown in the following table.
+//
+// | Word | A | B |
+// |-----:|---|---|
+// | 00   |Hi |Hi |
+// | 10   |Lo |Hi |
+// | 11   |Lo |Lo |
+// | 01   |Hi |Lo |
+//
+// Clockwise rotation of the knob starts with 00 as the initial state and the
+// 2-bit word transitions in the order of 01, 11, 10, 00. Counterclockwise
+// rotation of the knob starts with 00 as the initial state and the 2-bit word
+// transitions in the order of 10, 11, 01, 00.
+//
+// The transition history of this 2-bit word is represented by an 8-bit =
+// 1-byte variable using bit shift. Clockwise rotation is 0x1e (=00, 01, 11,
+// 10), and counterclockwise rotation is 0x2d (=00, 10, 11, 01). When the
+// latest 2-bit word is 00, the direction of rotation is determined by
+// comparing the value of one variable indicated by the transition history with
+// the aforementioned value, and whether the rotation was successful or not.
+
 typedef struct {
     uint8_t pinA;
     uint8_t pinB;
