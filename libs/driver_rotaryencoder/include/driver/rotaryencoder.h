2ce97c7114a29eda8d9d5cdc6c2bbf7529373457
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-09-29 11:47:57 +0900

    extract rotary encoder's driver into a separated directory

---
+++ libs/driver_rotaryencoder/include/driver/rotaryencoder.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <sys/types.h>
+
+typedef struct {
+    uint8_t pinA;
+    uint8_t pinB;
+    uint8_t history;
+    uint64_t changedAt;
+} rotary_encoder_t;
+
+void rotary_encoder_init(rotary_encoder_t* re, uint a, uint b);
+
+int8_t rotary_encoder_task(rotary_encoder_t *re, uint64_t now);
